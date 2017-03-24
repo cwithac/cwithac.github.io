61fe8e346e281f7c7fece919ae4ac62a8d34e0a5
Cathleen Wright <cathleenmwright@gmail.com>
2017-03-24 15:36:27 -0400

    Initial File Setup

---
+++ unit1/js/app.js
@@ -0,0 +1,10 @@
+if (typeof jQuery == 'undefined'){
+  console.log('oops! I still have to link my jQuery properly!');
+} else {console.log('I did it! I linked jQuery and this js file!')};
+
+$(function() {
+
+  // code inside jQuery window onload
+
+
+}); // closes window onload function
