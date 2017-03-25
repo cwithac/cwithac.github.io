d0d1444b907d69d8692322eb497b4719cc2d1c4b
Cathleen Wright <cathleenmwright@gmail.com>
2017-03-24 21:02:31 -0400

    Act 1 Questions in Object

--- unit1/js/questions.js
+++ unit1/js/questions.js
@@ -1,96 +1,6 @@
-// if (typeof jQuery == 'undefined'){
-//   console.log('oops! I still have to link my jQuery properly!');
-// } else {console.log('I did it! I linked jQuery and this js file!')};
+//DO NOT LINK THIS FILE, CONTENT IS ONLY FOR REFERENCE
+//LYRICS FROM https://genius.com/albums/Lin-manuel-miranda/Hamilton-original-broadway-cast-recording
 
-//======================================================================//
-//Window Onload Function
-
-$(function() {
-
-
-}); // closes window onload function
-
-//======================================================================//
-
-//PSEUDOCODE
-
-// //Characters
-// - Player 1 // Alexander Hamilton
-// - Player 2 // Aaron Burr
-//
-// //Visual
-// Container for Div P1 & Div P2
-// Div for Player 1 question
-// Div for Player 2 question
-// Score 1 Div
-// Score 2 Div
-// Reset Div
-// How to Div // Stretch
-//
-// //Content
-// questions = [
-//   { question: "Question 1", // Question 1...
-//     choices: ["A1", "A2", "A3"], // Array of possible answers
-//     correctAnswer: Index# // Index of correct Answer
-//   },
-//   { question: "Question 2", // Question 2...
-//     choices: ["A1", "A2", "A3"], // Array of possible answers
-//     correctAnswer: Index# // Index of correct Answer
-//   }
-// ];
-//
-// Text + Radio Button
-//
-// Player 1 Score = 0 //at start of game
-// Player 2 Score = 0 //at start of game
-//
-// question counter // tracks questions asked (if question < questions.length...)
-// user answers [] // tracks users answers (with push and .val() correct/not correct)
-//
-// How to Play (lorem ...)
-//
-// //Gameplay
-// Toggle // Switches Players after question has been asked
-// Player 1 Score +1 Question Correct +0 Incorrect
-// Player 2 Score +1 Question Correct +0 Incorrect
-//
-// //If selected = index, correct, if not, incorrect ...
-//
-// Each player turn, send to win eval.
-//
-// //Winner Eval
-// Checks if X questions have been asked, if not continue game with P1.
-// Checks between Player 1 and Player 2 Score for Winner
-// Winner Message
-//
-// //Reset Game
-// Starts game over without refresh (resets questions asked to 0, resets score)
-//
-// //CSS Stretch
-// Background Music
-
-//======================================================================//
-
-//Player Variables
-var player1 = {
-  name: "Alexander Hamilton",
-  score: 0,
-  answersGiven: []
-};
-
-var player2 = {
-  name: "Aaron Burr",
-  score: 0,
-  answersGiven: []
-};
-
-//Global Trackers
-var questionCounter = [];
-
-
-//======================================================================//
-
-//Questions
 var questions = [
   { question: "The world's gonna know your name. What’s your name, man?", // Question 1...
     choices: ["Alexander Hamilton.  My name is Alexander Hamilton.", "Aaron Burr.  My name is Aaron Burr.", "George Washington.  My name is George Washington."], // Array of possible answers
@@ -209,40 +119,18 @@ var questions = [
   }
 ];
 
-//======================================================================//
+//Index 0
+8
+//Index 1
+7
+//Index 2
+8
+//Total Songs
+23
 
-//Game Play
-
-//Player Actions
-var alexanderHamilton = function() {
-  console.log("alexanderHamilton function has been called.");
-  //Player 1 Turn
-  // aaronBurr();
-};
-
-var aaronBurr = function() {
-  console.log("aaronBurr function has been called.");
-  //Player 2 Turn
-  // evalWinner();
-};
-
-//Game Actions
-var evalWinner = function() {
-  console.log("evalWinner function has been called.");
-  //if questionCounter.length < questions.length
-  //continue game...
-  //else checkWinner();
-};
-
-var checkWinner = function() {
-  console.log("checkWinner function has been called.");
-  //which score is higher
-  //inform winner
-};
-
-var startGame = function() {
-  console.log("startGame function has been called.");
-  // alexanderHamilton();
-};
-
-startGame(); // Runs at Window Load, Disable to deactivate game
+,
+{ question: "", // Question...
+  choices: ["", "", ""], // Array of possible answers
+  correctAnswer: #, // Index of correct Answer
+  fromSong: ""
+}
