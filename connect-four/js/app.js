0b55d0bc18ee3bbb72bda22d38eb66b633659d0c
Cathleen Wright <cathleenmwright@gmail.com>
2017-07-19 18:53:53 -0400

    Moved Connect 4

---
+++ connect-four/js/app.js
@@ -0,0 +1,93 @@
+console.log('connect four app.js');
+
+//https://en.wikipedia.org/wiki/Connect_Four
+//The animation demonstrates Connect Four gameplay where the first player begins by dropping his/her yellow disc into the center column of the game board. The two players then alternate turns dropping one of their discs at a time into an unfilled column, until the second player, with red discs, achieves four discs in a row, diagonally, and wins. If the game board fills before either player achieves four in a row, then the game is a draw.
+
+//mandate bottom row to be filled first
+
+//https://github.com/edoorn/edoorn.github.io/blob/master/pandulce/js/app.js
+//Win cases
+
+//---------------------------------------------------------//
+
+$(() => { // Window Onload
+
+  UI.createGameBoard();
+
+}); //End of Window Onload
+
+//---------------------------------------------------------//
+
+//Global variables
+
+  let redTurn = true;
+  let played = false;
+  let legalMove = false;
+
+//---------------------------------------------------------//
+
+//User Interface Objects
+
+const UI = {
+  createGameBoard(){
+    //Generates 6x7 game board of squares
+      for (let i = 0; i < 42; i++) {
+          const $gameBoard = $('#gameboard');
+          const $square = $('<div>').attr('class', 'emptySpace').attr('id', i);
+          $gameBoard.append($square).eq(i);
+            if (i > 34) {
+              $square.addClass('canBePlayed');
+            }
+          $square.on('click', PA.playPiece);
+        };
+        this.whoseTurnIsIt();
+      },
+    whoseTurnIsIt() {
+      //Informs user if it is red turn or black turn
+      const $whoseTurn = $('#whose-turn')
+      if (redTurn) {
+        $whoseTurn.text("It is the red player's turn!");
+      } else {
+        $whoseTurn.text("It is the black player's turn!");
+      }
+    }
+  }; //End UI object
+
+//---------------------------------------------------------//
+
+//Player Action Objects
+
+  const PA = {
+    checkForLegalMove(e) {
+      //Confirms 'gravity', fill from bottom to top
+      const $targetIDAbove = parseInt($(e.currentTarget).attr('id'));
+      const $openSpace = $(e.currentTarget).hasClass('canBePlayed')
+      const $spaceAbove = $('#' + ($targetIDAbove - 7));
+        if ($openSpace) {
+          $spaceAbove.addClass('canBePlayed');
+          legalMove = true;
+        } else {
+          legalMove = false;
+          $message = $('<div>').text('illegal move');
+          $message.insertAfter('#whose-turn'); //repeating
+        };
+    },
+    playPiece(e) {
+      PA.checkForLegalMove(e);
+      //Plays red and black alternating
+      if (legalMove) {
+        played = false;
+        if (redTurn && !played) {
+          $(e.currentTarget).attr('class', 'redChip').addClass('played').removeClass('emptySpace');
+          redTurn = false;
+          played = true;
+          UI.whoseTurnIsIt();
+        } else {
+          $(e.currentTarget).attr('class', 'blackChip').addClass('played').removeClass('emptySpace');
+          redTurn = true;
+          played = true;
+          UI.whoseTurnIsIt();
+        };
+      };
+    }
+  }; //End PA Object
