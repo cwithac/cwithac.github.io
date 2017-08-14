f5270f3bb5b0d6df2554fb1325350e8917830460
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-14 19:25:41 -0400

    For loop for jquery

--- js/app.js
+++ js/app.js
@@ -2,58 +2,16 @@
 
 $( () => {
 
-//Creative Juices
-	$('#creativejuices-header').on('mouseover', () => {
-		$('#creativejuices-description').css('display', 'inherit');
-	});
+	const headers = ['#creativejuices-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header'];
+	const descriptions = ['#creativejuices-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description'];
 
-	$('#creativejuices-header').on('mouseout', () => {
-		$('#creativejuices-description').css('display', 'none');
-	});
-
-//The Perfect Excuse
-	$('#perfectexcuse-header').on('mouseover', () => {
-		$('#perfectexcuse-description').css('display', 'inherit');
-	});
-
-	$('#perfectexcuse-header').on('mouseout', () => {
-		$('#perfectexcuse-description').css('display', 'none');
-	});
-
-//Connect Four
-	$('#connectfour-header').on('mouseover', () => {
-		$('#connectfour-description').css('display', 'inherit');
-	});
-
-	$('#connectfour-header').on('mouseout', () => {
-		$('#connectfour-description').css('display', 'none');
-	});
-
-//99 Bottles of Beer on the Wall
-	$('#beerswall-header').on('mouseover', () => {
-		$('#beerswall-description').css('display', 'inherit');
-	});
-
-	$('#beerswall-header').on('mouseout', () => {
-		$('#beerswall-description').css('display', 'none');
-	});
-
-//The Disney Vault
-	$('#disneyvault-header').on('mouseover', () => {
-		$('#disneyvault-description').css('display', 'inherit');
-	});
-
-	$('#disneyvault-header').on('mouseout', () => {
-		$('#disneyvault-description').css('display', 'none');
-	});
-
-//Lyrics
-	$('#lyrics-header').on('mouseover', () => {
-		$('#lyrics-description').css('display', 'inherit');
-	});
-
-	$('#lyrics-header').on('mouseout', () => {
-		$('#lyrics-description').css('display', 'none');
-	});
+	for (let i = 0; i < headers.length; i++){
+		$(headers[i]).on('mouseover', () => {
+			$(descriptions[i]).css('display', 'inherit');
+		});
+		$(headers[i]).on('mouseout', () => {
+			$(descriptions[i]).css('display', 'none');
+		});
+	};
 
 });
