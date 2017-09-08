727e7a35d730e0f412075245ff80872073012c63
Cathleen Wright <cathleenmwright@gmail.com>
2017-09-07 22:39:45 -0400

    Mobile remove animation

--- js/app.js
+++ js/app.js
@@ -2,16 +2,21 @@
 
 $( () => {
 
-	const headers = ['#creativejuices-header', '#colorthes-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header'];
-	const descriptions = ['#creativejuices-description', '#colorthes-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description'];
+	if (Modernizr.mq('(max-width: 500px)')) {
+		
+		const headers = ['#creativejuices-header', '#colorthes-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header'];
+		const descriptions = ['#creativejuices-description', '#colorthes-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description'];
 
-	for (let i = 0; i < headers.length; i++){
-		$(headers[i]).on('mouseover', () => {
-			$(descriptions[i]).css('display', 'inherit');
-		});
-		$(headers[i]).on('mouseout', () => {
-			$(descriptions[i]).css('display', 'none');
-		});
-	};
+		for (let i = 0; i < headers.length; i++){
+			$(headers[i]).on('mouseover', () => {
+				$(descriptions[i]).css('display', 'inherit');
+			});
+			$(headers[i]).on('mouseout', () => {
+				$(descriptions[i]).css('display', 'none');
+			});
+		};
+}
+
+
 
 });
