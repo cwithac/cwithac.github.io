8f3c464c532ebf7d96e285eb876b0cb0ead30299
Cathleen Wright <cathleenmwright@gmail.com>
2017-10-27 19:34:38 -0400

    Javascript Cleanup

--- games/landscaper/app.js
+++ games/landscaper/app.js
@@ -1,52 +1,60 @@
-// console.log('Landscaper Game app.js is attached to index.html');
-
 $( () => {
 
   //Page Load Invocation
   startGame();
 
-}); // closes window onload
+}); //
 
-// Variable Setup
+// console.log('Landscaper Game app.js is attached to index.html');
+
+//INITIALIZE
 let tool;
 let money;
+let cost;
+let choice;
 
-const resetVariables = () => {
-  tool = '';
-  money = 0;
-};
-
-const levelOneVariableSetup = () => {
+const startGame = () => {
   tool = "your teeth";
   money = 1;
-};
-
-// Game Functionality
-const startGame = () => {
-  levelOneVariableSetup();
-  console.log("Welcome to the game!  You are starting a landscaping business.  Use your tools to earn more money!");
+  // instructions();
   // askForAction();
 };
 
+// const instructions = () => {
+//   alert("Welcome to the game!  You are starting a landscaping business.  Use your tools to earn more money!")
+//   alert("You win the game when you have a team of starving students helping and $1000.")
+//   alert("To start, the only tool you have are your teeth. Using just " + tool + ", you can spend the day cutting lawns and make $" + money + ".  You can do this as much as you want.");
+// };
+//
+// //USER INFORMATION
 // const showStatus = () => {
 //   alert("You have $" + money + ".  Your tool is " + tool + ".")
 // };
 //
+// //USER ACTIONS AND INPUT
 // const askForAction = () => {
 //   showStatus();
-//   const choice = prompt("What do you want to do?", "landscape/buy tools");
-//     if (choice === "landscape") {
+//   promptQuestions();
+// };
+//
+// const promptQuestions = () => {
+//   choice = prompt("What do you want to do?", "landscape/buy tools");
+//     if (choice.toLowerCase() === "landscape") {
 //       landscape();
-//     } else if (choice === "buy tools") {
+//     } else if (choice.toLowerCase() === "buy tools") {
 //       checkTool();
-//     } else if (chioce === "exit") {
+//     } else if (choice.toLowerCase() === "exit") {
+//       //exits game - option not shown to user
+//     } else if (choice.toLowerCase() === "restart") {
+//       //resets game - option not shown to user
+//       start();
 //     } else {
 //       alert("Please try that action again!")
 //       askForAction();
 //     }
 // };
 //
-//
+// //LANDSCAPE ACTIONS
 // const landscape = () => {
 //   if (tool === "your teeth") {
 //     alert("Using " + tool + " will earn you $1 each day!")
@@ -66,12 +74,14 @@ const startGame = () => {
 //       if (money <1000 ) {
 //         askForAction();
 //       } else {
+//         //Game Ends
 //         alert("Congratulations!  You have made $" + money + " with the help of your tools!  You have won the game!")
 //       }
 //   }
 // };
 //
 //
+// //CHECK AND BUY TOOLS
 // const checkTool = function () {
 //   if (money < 5) {
 //     alert("Sorry, you don't have enough money to buy a new tool yet.  Keep landscaping!");
@@ -100,34 +110,59 @@ const startGame = () => {
 //   }
 // };
 //
+//
+// //BUY FUNCTIONS
 // const buyScissors = () => {
-//   alert("You can buy a pair of rusty scissors for $5.")
+//   cost = 5;
 //   money -= 5;
 //   tool = "a pair of rusty scissors";
-//   alert("You have purchased " + tool + "!  Using this tool will earn you $5 each day!")
-//   askForAction();
+//   alertText();
 // };
 //
 // const buyOldTimey = () => {
-//   alert("You can buy an old-timey push lawnmower for $25.")
+//   cost = 25;
 //   money -= 25;
 //   tool = "an old-timey push lawnmower";
-//   alert("You have purchased " + tool + "!  Using this tool will earn you $50 each day!")
-//   askForAction();
+//   alertText();
 // };
 //
 // const buyFancyBattery = () => {
-//   alert("You can buy a fancy battery-powered lawnmower for $250.")
+//   cost = 250;
 //   money -= 250;
 //   tool = "a fancy battery-powered lawnmower";
-//   alert("You have purchased " + tool + "!  Using this tool will earn you $100 each day!")
-//   askForAction();
+//   alertText();
 // };
 //
 // const buyTeamOfStudents = () => {
-//   alert("You can hire a team of starving students for $500.")
+//   cost = 500;
 //   money -= 500;
 //   tool = "a team of starving students";
-//   alert("You have purchased " + tool + "!  Using this tool will earn you $250 each day!")
+//   alertText();
+// };
+//
+// const alertText = () => {
+//   alert("You can buy " + tool + " for $" + cost + ".");
+//   alert("You have purchased " + tool + "!  Using this tool will earn you $" + cost + " each day!");
 //   askForAction();
 // };
+//
+// // PAGE LOAD FUNCTION CALL
+// startGame();
+
+//==============================================================================
+
+// How to Play
+//
+// Reload the page to launch the game.
+//
+// You are starting a landscaping business, but all you have are your teeth. Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
+//
+// At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money. Using the rusty scissors, you can spend the day cutting lawns and make $5. You can do this as much as you want.
+//
+// At any point, if you are currently using rusty scissors, you can buy an old-timey push lawnmower for $25. You can do this once, assuming you have enough money. Using the old-timey push lawnmower, you can spend the day cutting lawns and make $50. You can do this as much as you want.
+//
+// At any point, if you are currently using the old-timey push lawnmower, you can buy a fancy battery-powered lawnmower for $250. You can do this once, assuming you have enough money. Using the the fancy battery-powered lawnmower, you can spend the day cutting lawns and make $100. You can do this as much as you want.
+//
+// At any point, if you are currently using the fancy battery-powered lawnmower, you can hire a team of starving students for $500. You can do this once, assuming you have enough money. Using the the team of starving students, you can spend the day cutting lawns and make $250. You can do this as much as you want.
+//
+// You win the game when you have a team of starving students and $1000.
