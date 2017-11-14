91f3125855fa7346b919fc80a15b7db7b0646ba5
Cathleen Wright <cathleenmwright@gmail.com>
2017-11-13 22:07:02 -0500

    Add landscaper to portfolio

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ $( () => {
 
 	if (windowWidth > 667) {
 
-		const headers = ['#creativejuices-header', '#colorthes-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header', '#notable-header'];
-		const descriptions = ['#creativejuices-description', '#colorthes-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description', '#notable-description'];
+		const headers = ['#creativejuices-header', '#colorthes-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header', '#notable-header', '#landscaper-header'];
+		const descriptions = ['#creativejuices-description', '#colorthes-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description', '#notable-description', '#landscaper-description'];
 
 		for (let i = 0; i < headers.length; i++){
 			$(headers[i]).on('mouseover', () => {
