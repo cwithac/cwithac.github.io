ccd9416cfb35f8b7ef4ae217c1f68b7b7ee1507f
Cathleen Wright <cathleenmwright@gmail.com>
2017-11-25 20:54:15 -0500

    Move games and apps to main page with jquery

--- js/app.js
+++ js/app.js
@@ -11,14 +11,50 @@ $( () => {
 		const headers = ['#creativejuices-header', '#colorthes-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header', '#notable-header', '#landscaper-header'];
 		const descriptions = ['#creativejuices-description', '#colorthes-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description', '#notable-description', '#landscaper-description'];
 
-		for (let i = 0; i < headers.length; i++){
-			$(headers[i]).on('mouseover', () => {
-				$(descriptions[i]).css('display', 'inherit');
-			});
-			$(headers[i]).on('mouseout', () => {
-				$(descriptions[i]).css('display', 'none');
-			});
-		};
-}
+			for (let i = 0; i < headers.length; i++){
+				$(headers[i]).on('mouseover', () => {
+					$(descriptions[i]).css('display', 'inherit');
+				});
+				$(headers[i]).on('mouseout', () => {
+					$(descriptions[i]).css('display', 'none');
+				});
+			};
+	};
+
+const $gamesSection = $('#games-section');
+const $gamesLaunch = $('#games-launch');
+const $appsSection = $('#apps-section');
+const $appsLaunch = $('#apps-launch');
+const $backArrow = $('.fa-reply');
+
+const windowLoadView = () => {
+	$gamesSection.hide();
+	$appsSection.hide();
+	$gamesLaunch.show();
+	$appsLaunch.show();
+};
+
+const hideLinksView = () => {
+	$gamesLaunch.hide();
+	$appsLaunch.hide();
+	$gamesLaunch.parent().hide();
+};
+
+$backArrow.on('click', () => {
+	$gamesLaunch.parent().show();
+	windowLoadView();
+});
+
+$gamesLaunch.on('click', () => {
+	hideLinksView();
+	$gamesSection.show();
+});
+
+$appsLaunch.on('click', () => {
+	hideLinksView();
+	$appsSection.show();
+});
+
+windowLoadView();
 
 });
