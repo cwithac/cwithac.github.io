24ff2ef837e2b9514b9bee7d2c3b7dd6ce4d5ddb
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-21 09:40:47 -0400

    Warbler Add

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ $( () => {
 
 	if (windowWidth > 667) {
 
-		const headers = ['#creativejuices-header', '#colorthes-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header', '#notable-header', '#landscaper-header', '#vvdc-header', '#yelpcamp-header', '#loremroses-header', '#rgb-header', '#patatap-header', '#wonderland-header', '#rogers-header', '#slotify-header', '#social-header'];
+		const headers = ['#creativejuices-header', '#colorthes-header', '#perfectexcuse-header', '#connectfour-header', '#beerswall-header', '#disneyvault-header', '#lyrics-header', '#notable-header', '#landscaper-header', '#vvdc-header', '#yelpcamp-header', '#loremroses-header', '#rgb-header', '#patatap-header', '#wonderland-header', '#rogers-header', '#slotify-header', '#social-header', '#warbler-header'];
 
-		const descriptions = ['#creativejuices-description', '#colorthes-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description', '#notable-description', '#landscaper-description', '#vvdc-description', '#yelpcamp-description', '#loremroses-description', '#rgb-description', '#patatap-description', '#wonderland-description', '#rogers-description', '#slotify-description', '#social-description'];
+		const descriptions = ['#creativejuices-description', '#colorthes-description', '#perfectexcuse-description', '#connectfour-description', '#beerswall-description', '#disneyvault-description', '#lyrics-description', '#notable-description', '#landscaper-description', '#vvdc-description', '#yelpcamp-description', '#loremroses-description', '#rgb-description', '#patatap-description', '#wonderland-description', '#rogers-description', '#slotify-description', '#social-description', '#warbler-description'];
 
 			for (let i = 0; i < headers.length; i++){
 				$(headers[i]).on('mouseover', () => {
